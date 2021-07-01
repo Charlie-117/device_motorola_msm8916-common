8ef37d5eb35980946d4b1b3a3bf1952d9b343f75
Tony Jose <gmoto1179@gmail.com>
2021-07-01 19:29:19 +0530

    msm8916-common: inherit custom configs
    Change-Id: Iee0e8e58445237974c8473650ee869502fc35f85

--- msm8916.mk
+++ msm8916.mk
@@ -359,4 +359,5 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 $(call inherit-product, vendor/motorola/msm8916-common/msm8916-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, vendor/mmods/product.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
